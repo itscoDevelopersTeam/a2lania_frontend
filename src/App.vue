e894490b3e84e25241fa1e1a80a104dea83adb8f
Samuel Gómez Balderas <sierra_034@outlook.com>
2019-08-26 16:16:27 -0500

    app basic template

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,71 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+	<v-app>
+		<!-- Barra de navegación -->
+		<v-navigation-drawer app>
+			<v-list-item>
+				<v-list-item-content>
+					<v-list-item-title class="title">
+						Application
+					</v-list-item-title>
+					<v-list-item-subtitle>
+						subtext
+					</v-list-item-subtitle>
+				</v-list-item-content>
+			</v-list-item>
+	
+			<v-divider></v-divider>
+	
+			<v-list dense nav>
+				<v-list-item v-for="item in items" :key="item.title" link>
+					<v-list-item-icon>
+						<v-icon>{{ item.icon }}</v-icon>
+					</v-list-item-icon>
+		
+					<v-list-item :to="item.route">
+						<v-list-item-title>{{ item.title }}</v-list-item-title>
+					</v-list-item>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
 
-    <v-content>
-      <router-view/>
-    </v-content>
-  </v-app>
+		<!-- Barra de título -->
+		<v-app-bar app>
+			<v-toolbar-title class="headline text-uppercase">
+				<span>A2LANIA</span>
+				<span class="font-weight-light">Home</span>
+			</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-btn
+				text
+				href="https://github.com/vuetifyjs/vuetify/releases/latest"
+				target="_blank"
+			>
+				<span class="mr-2">Latest Release</span>
+				<v-icon>mdi-open-in-new</v-icon>
+			</v-btn>
+		</v-app-bar>
+
+		<v-content>
+			<router-view/>
+		</v-content>
+	</v-app>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
-  data: () => ({
-    //
-  }),
+	name: 'App',
+	data() {
+		return {
+			items: [
+				{ title: 'Cursos', icon: 'mdi-view-dashboard', route: 'cursos' },
+				{ title: 'Reportes', icon: 'mdi-file-chart', route: 'reportes' },
+				{ title: 'Cierre', icon: 'mdi-close-circle', route: 'cierre' },
+				{ title: 'Diplomas', icon: 'mdi-newspaper-variant-outline', route: 'diplomas' },
+				{ title: 'Calendario', icon: 'mdi-calendar', route: 'calendario' },
+        	],
+        	right: null,
+		}
+	},
 };
 </script>
